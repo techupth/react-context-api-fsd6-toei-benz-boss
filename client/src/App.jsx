f19865100250004a5f95328a871f1ce34b1c313c
paphonwat <paphonwat.w@gmail.com>
2024-06-14 14:13:52 +0700

    feat:sent React React Context Api Assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+export const UserDataContext = React.createContext();
 
 function App() {
   const userData = {
@@ -13,13 +16,19 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserDataContext.Provider
+      value={{
+        user: userData,
+      }}
+    >
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+    </UserDataContext.Provider>
+    </div>  
   );
 }
 
